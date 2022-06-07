653e92de648a2ed6dfbeadf4fcf3a5994f2eb3c9
Constanza Arreche <constanzaarreche@gmail.com>
2022-06-07 17:10:42 -0300

    Segunda entrega update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,33 @@
-// Disclaimer inicial
-alert("Le presentamos las opciones disponibles dentro de Chillin'")
-
 // Defino la variable de tipo de café
 let coffeeType; 
 
 // Listado de cafés disponibles
 const coffees = [
     {
-        id: 1,
-        image: "",
+        id: "icedvanillalatte",
+        image: "../assets/img/product_3.png",
         name: "Iced Vanilla Latte",
         description: "Bebida a base de espresso y jarabe de vainilla con hielo.",
         price: "$550"
     },
     {
-        id: 2,
-        image: "",
+        id: "carameltoffeelatte",
+        image: "../assets/img/product_2.png",
         name: "Caramel Toffee Latte",
         description: "Bebida a base de café, salsa de caramelo, toffee y canela.",
         price: "$600"
 
     },
     {
-        id: 3,
-        image: "",
+        id: "flatwhite",
+        image: "../assets/img/product_1.png",
         name: "Flat White",
         description: "Bebida a base de espresso que contiene leche cremada.",
         price: "$610"
     },
     {
-        id: 4,
-        image: "",
+        id: "capuccino",
+        image: "../assets/img/product_4.png",
         name: "Capuccino",
         description: "Bebida preparada con café espreso y leche montada con vapor.",
         price: "$400"
@@ -41,44 +38,47 @@ const coffees = [
 const events = [
     {
         id: 1,
-        image: "",
+        image: "../assets/img/events_4.jpg",
         name: "Coffee Night",
         description: "Vení a degustar lo mejor del café y conocer las sorpresas que tenemos para vos.",
-        date: "Día y horario: 10 de Agosto 2022 - 21:00hs",
-        address: "Dirección: Cuyo 1590, Martínez, Provincia de Buenos Aires"
+        date: "10 de Agosto 2022 - 21:00hs",
+        address: "Cuyo 1590, Martínez, Provincia de Buenos Aires"
     },
     {
         id: 2,
-        image: "",
+        image: "../assets/img/events_1.jpg",
         name: "Beer & Friends",
         description: "Noche de cerveza de gran variedad, en conjunto con las mejores hamburguesas para todos los gustos.",
-        date: "Día y horario: 4 de Septiembre 2022 - 21:30hs",
-        address: "Dirección: Cuyo 1590, Martínez, Provincia de Buenos Aires"
+        date: "4 de Septiembre 2022 - 21:30hs",
+        address: "Cuyo 1590, Martínez, Provincia de Buenos Aires"
     },
     {
         id: 3,
-        image: "",
+        image: "../assets/img/events_2.jpg",
         name: "Show Night",
         description: "Junto a un espectáculo secreto que tenemos para vos, te ofrecemos platos de gran calidad, bebida de variedad y pasar un momento inolvidable.",
-        date: "Día y horario: 17 de Septiembre 2022 - 22:00hs",
-        address: "Dirección: Cuyo 1590, Martínez, Provincia de Buenos Aires"
+        date: "17 de Septiembre 2022 - 22:00hs",
+        address: "Cuyo 1590, Martínez, Provincia de Buenos Aires"
     },
     {
         id: 4,
-        image: "",
+        image: "../assets/img/events_3.jpg",
         name: "Wine & Dine",
         description: "Te invitamos a probar los mejores vinos y platos gourmet que tenemos para vos.",
-        date: "Día y horario: 6 de Octubre 2022 - 21:30hs",
-        address: "Dirección: Cuyo 1590, Martínez, Provincia de Buenos Aires"
+        date: "6 de Octubre 2022 - 21:30hs",
+        address: "Cuyo 1590, Martínez, Provincia de Buenos Aires"
     }
-]
+];
+
+
 
 // Función para hacer la selección por prompts y alerts
-function selectOption() {
+/*function selectOption() {
     
     let optionSelected = '';
     
-    while(optionSelected !== 'Ninguna') {
+    if(optionSelected !== 'Ninguna') {
+
         optionSelected = prompt('Listado de acciones, escriba la que desea: \n\nListado de cafés\nComprar café\nListado de eventos\nNinguna');
     
         switch(optionSelected) {
@@ -106,59 +106,51 @@ function selectOption() {
             }
         }
     }
-}
-
-
-// Función para listar cafés
-function listCoffee() {
-    alert ("Te presentamos el listado de cafés:");
-
-    coffees.forEach(
-        (coffee)=>{
-            alert(coffee.name + "\n" + coffee.description + "\n" + coffee.price);
-        }
-    );
-}
+}*/
 
 // Función para comprar café
-function totalCoffee(quantity){
+/*function totalCoffee(quantity){
+
+    const modalTienda = document.querySelector("#modal_tienda");
+    const modalTiendaTotal = document.querySelector("#tienda-total");
+
     while (quantity != 0){
         if (coffeeType == 'Iced Vanilla Latte') {
             let coffeeTotal = quantity * 550;
-            alert('Debe abonar $' + coffeeTotal + '\nMuchas gracias por su compra');
+            modalTiendaTotal.innerHTML = "<b>Usted debe abonar:</b> $" + coffeeTotal;
             break;
         }
         
         if (coffeeType == 'Caramel Toffee Latte') {
             let coffeeTotal = quantity * 600;
-            return('Debe abonar $' + coffeeTotal + '\nMuchas gracias por su compra');
+            modalTiendaTotal.innerHTML = "<b>Usted debe abonar:</b> $" + coffeeTotal;
             break;
         }
     
         if (coffeeType == 'Flat White') {
             let coffeeTotal = quantity * 610;
-            alert('Debe abonar $' + coffeeTotal + '\nMuchas gracias por su compra');
+            modalTiendaTotal.innerHTML = "<b>Usted debe abonar:</b> $" + coffeeTotal;
             break;
         }
 
         if (coffeeType == 'Capuccino') {
             let coffeeTotal = quantity * 400;
-            alert('Debe abonar $' + coffeeTotal + '\nMuchas gracias por su compra');
+            modalTiendaTotal.innerHTML = "<b>Usted debe abonar:</b> $" + coffeeTotal;
             break;
         }
     }
+
+    document.body.appendChild(modalTienda);
+    modalTienda.style.display = "block";
 }
+*/
 
-// Función para listar eventos
-function listEvents() {
-    alert ("Te presentamos el listado de eventos:");
 
-    events.forEach(
-        (event)=>{
-            alert(event.name + "\n" + event.description + "\n" + event.date + "\n" + event.address);
-        }
-    );
-}
+// Listado de cafés
+listCoffee();
 
-// Opciones a seleccionar por el usuario
-selectOption();
+// Listado de eventos
+listEvents();
+
+// Abrir carrito
+openCart();
